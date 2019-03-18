4c93d38848b5064bf8f8145643070ca2aee80efe
mochajisukmawan <ajie.sukmawan@gmail.com>
2019-03-18 16:20:18 +0700

    setengah jadi login

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -1,374 +1,95 @@
-function is_login(callback){
-  var datas = new FormData();
-  datas.append("user_name", "NR.000012");
-  datas.append("user_password", "taekwondo");
-
-  $.ajax({
-     type: "POST",
-     url: "http://magang.bankjateng.co.id/api/loginpeserta",
-     data: datas,
-     processData: false,
-     contentType: false,
-     success: function(data) {
-       if(data.error == true){
-         if (typeof callback == "function") {
-             callback(data);
-         }
-       }
-     },
-     error: function(data) {
-       if (typeof callback == "function") {
-           callback(data);
-       }
-     }
-   });
-}
-
-
-  var routes = [
-  {
-    path: '/',
-    url: './index.html',
-    on: {
-        pageBeforeIn: function(event, page) {
-
-        },
-        pageAfterIn: function(event, page) {
-          console.log("index after in");
-        },
-        pageInit: function(event, page) {
-          is_login(function(data){
-            var app = page.app
-            app.router.navigate('/login/');
-          });
-        },
-        pageBeforeRemove: function(event, page) {
-          console.log("index before leave");
-        },
-    }
-  },
-  {
-    path: '/login/',
-    url: './pages/login.html',
-  },
-  {
-    path: '/register/',
-    url: './pages/index.html',
-  },
-  {
-      path: '/forgot-password/',
-      url: './pages/forgot-password.html',
-  },
-  {
-      path: '/employees/',
-      url: './pages/employees.html',
-  },
-  {
-      path: '/add-employee/',
-      url: './pages/add-employee.html',
-  },
-  {
-      path: '/edit-employee/',
-      url: './pages/edit-employee.html',
-  },
-  {
-      path: '/clients/',
-      url: './pages/clients.html',
-  },
-  {
-      path: '/add-client/',
-      url: './pages/add-client.html',
-  },
-  {
-      path: '/edit-client/',
-      url: './pages/edit-client.html',
-  },
-  {
-      path: '/leaves/',
-      url: './pages/leaves.html',
-  },
-  {
-      path: '/add-leave/',
-      url: './pages/add-leave.html',
-  },
-  {
-      path: '/leave-view/',
-      url: './pages/leave-view.html',
-  },
-  {
-      path: '/holidays/',
-      url: './pages/holidays.html',
-  },
-  {
-      path: '/add-holiday/',
-      url: './pages/add-holiday.html',
-  },
-  {
-      path: '/edit-holiday/',
-      url: './pages/edit-holiday.html',
-  },
-  {
-      path: '/departments/',
-      url: './pages/departments.html',
-  },
-  {
-      path: '/add-department/',
-      url: './pages/add-department.html',
-  },
-  {
-      path: '/edit-department/',
-      url: './pages/edit-department.html',
-  },
-  {
-      path: '/designations/',
-      url: './pages/designations.html',
-  },
-  {
-      path: '/add-designation/',
-      url: './pages/add-designation.html',
-  },
-  {
-      path: '/edit-designation/',
-      url: './pages/edit-designation.html',
-  },
-  {
-      path: '/users/',
-      url: './pages/users.html',
-  },
-  {
-      path: '/add-user/',
-      url: './pages/add-user.html',
-  },
-  {
-      path: '/edit-user/',
-      url: './pages/edit-user.html',
-  },
-  {
-      path: '/email/',
-      url: './pages/email.html',
-  },
-  {
-      path: '/email-list/',
-      url: './pages/email-list.html',
-  },
-  {
-      path: '/email-view/',
-      url: './pages/email-view.html',
-  },
-  {
-      path: '/profile/',
-      url: './pages/profile.html',
-
-  },
-  {
-      path: '/projects/',
-      url: './pages/projects.html',
-  },
-  {
-      path: '/add-project/',
-      url: './pages/add-project.html',
-  },
-  {
-      path: '/edit-project/',
-      url: './pages/edit-project.html',
-  },
-  {
-      path: '/project-view/',
-      url: './pages/project-view.html',
-  },
-  {
-      path: '/tasks/',
-      url: './pages/tasks.html',
-  },
-  {
-      path: '/task-view/',
-      url: './pages/task-view.html',
-  },
-  {
-      path: '/contacts/',
-      url: './pages/contacts.html',
-  },
-  {
-      path: '/walkthrough/',
-      url: './pages/walkthrough.html',
-  },
-  {
-      path: '/chat/',
-      url: './pages/chat.html',
-  },
-  {
-      path: '/chat-view/',
-      url: './pages/chat-view.html',
-  },
-  {
-      path: '/invoices/',
-      url: './pages/invoices.html',
-  },
-  {
-      path: '/create-invoice/',
-      url: './pages/create-invoice.html',
-  },
-  {
-      path: '/edit-invoice/',
-      url: './pages/edit-invoice.html',
-  },
-  {
-      path: '/invoice-view/',
-      url: './pages/invoice-view.html',
-  },
-  {
-      path: '/estimates/',
-      url: './pages/estimates.html',
-  },
-  {
-      path: '/create-estimate/',
-      url: './pages/create-estimate.html',
-  },
-  {
-      path: '/edit-estimate/',
-      url: './pages/edit-estimate.html',
-  },
-  {
-      path: '/estimate-view/',
-      url: './pages/estimate-view.html',
-  },
-  {
-      path: '/payments/',
-      url: './pages/payments.html',
-  },
-  {
-      path: '/salary/',
-      url: './pages/salary.html',
-  },
-  {
-      path: '/add-salary/',
-      url: './pages/add-salary.html',
-  },
-  {
-      path: '/edit-salary/',
-      url: './pages/edit-salary.html',
-  },
-  {
-      path: '/salary-view/',
-      url: './pages/salary-view.html',
-  },
-  {
-      path: '/taxes/',
-      url: './pages/taxes.html',
-  },
-  {
-      path: '/add-tax/',
-      url: './pages/add-tax.html',
-  },
-  {
-      path: '/edit-tax/',
-      url: './pages/edit-tax.html',
-  },
-  {
-      path: '/voice-call/',
-      url: './pages/voice-call.html',
-  },
-  {
-      path: '/video-call/',
-      url: './pages/video-call.html',
-  },
-  {
-      path: '/incoming-call/',
-      url: './pages/incoming-call.html',
-  },
-  {
-      path: '/notifications/',
-      url: './pages/notifications.html',
-  },
-  {
-      path: '/settings/',
-      url: './pages/settings.html',
-  },
-  {
-      path: '/company-settings/',
-      url: './pages/company-settings.html',
-  },
-  {
-      path: '/localization/',
-      url: './pages/localization.html',
-  },
-  {
-      path: '/email-settings/',
-      url: './pages/email-settings.html',
-  },
-  {
-      path: '/change-password/',
-      url: './pages/change-password.html',
-  },
-  {
-      path: '/notifications-settings/',
-      url: './pages/notifications-settings.html',
-  },
-  {
-      path: '/salary-settings/',
-      url: './pages/salary-settings.html',
-  },
-  {
-      path: '/invoice-settings/',
-      url: './pages/invoice-settings.html',
-  },
-  {
-      path: '/leave-types/',
-      url: './pages/leave-types.html',
-  },
-  {
-      path: '/add-leavetype/',
-      url: './pages/add-leavetype.html',
-  },
-  {
-      path: '/edit-leavetype/',
-      url: './pages/edit-leavetype.html',
-  },
-  {
-    path: '/menu-absen/',
-    url: './pagesikeren/absen/menu-absen.html',
-    async(routeTo, routeFrom, resolve, reject) {
-      is_login(function(data){
-        resolve({ url: 'pages/login.html' });
-      });
-    } ,
-    on: {
-        pageBeforeIn: function(event, page) {
-          console.log("index before in");
-        },
-        pageAfterIn: function(event, page) {
-          console.log("index after in");
-        },
-        pageInit: function(event, page) {
-          console.log("index in");
-        },
-        pageBeforeRemove: function(event, page) {
-          console.log("index before leave");
-        },
-    }
-  },
-  {
-    path: '/absen-pagi/',
-    url: './pagesikeren/absen/absen-pagi.html',
-  },
-  {
-    path: '/absen-sore/',
-    url: './pagesikeren/absen/absen-sore.html',
-  },
-  {
-    path: '/uang-saku/',
-    url: './pagesikeren/uangsaku/uang-saku.html',
-  },
-  {
-    path: '/kehadiran/',
-    url: './pagesikeren/kehadiran/kehadiran.html',
-  },
-  {
-    path: '/raport/',
-    url: './pagesikeren/raport/raport.html',
-  },
-  {
-    path: '/kinerja-harian/',
-    url: './pagesikeren/kinerjaharian/kinerja-harian.html',
+var routes = [
+{
+  path: '/',
+  url: './index.html',
+  on: {
+      pageBeforeIn: function(event, page) {
+      },
+      pageAfterIn: function(event, page) {
+        console.log("index after in");
+      },
+      pageInit: function(event, page) {
+        is_login(function(data){
+          var app = page.app
+          app.router.navigate('/login/');
+        });
+      },
+      pageBeforeRemove: function(event, page) {
+        console.log("index before leave");
+      },
   }
+},
+{
+  path: '/login/',
+  url: './pages/login.html',
+},
+{
+  path: '/menu-absen/',
+  url: './pagesikeren/absen/menu-absen.html',
+  async(routeTo, routeFrom, resolve, reject) {
+    is_login(function(data){
+      resolve({ url: 'pages/login.html' });
+    });
+  },
+  on: {
+      pageBeforeIn: function(event, page) {
+        console.log("index before in");
+      },
+      pageAfterIn: function(event, page) {
+        console.log("index after in");
+      },
+      pageInit: function(event, page) {
+        console.log("index in");
+      },
+      pageBeforeRemove: function(event, page) {
+        console.log("index before leave");
+      },
+  }
+},
+{
+  path: '/absen-pagi/',
+  url: './pagesikeren/absen/absen-pagi.html',
+  async(routeTo, routeFrom, resolve, reject) {
+    is_login(function(data){
+      resolve({ url: 'pages/login.html' });
+    });
+  },
+  on: {
+      pageBeforeIn: function(event, page) {
+        console.log("index before in");
+      },
+      pageAfterIn: function(event, page) {
+        var d = new Date();
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+        $("#kethari").html(days[d.getDay()]+", "+d.getDate()+" "+months[d.getMonth()]+" "+d.getFullYear()+" Pukul : "+d.getHours()+":"+d.getMinutes());
+      },
+      pageInit: function(event, page) {
+        console.log("index in");
+      },
+      pageBeforeRemove: function(event, page) {
+        console.log("index before leave");
+      },
+  }
+},
+{
+  path: '/absen-sore/',
+  url: './pagesikeren/absen/absen-sore.html',
+},
+{
+  path: '/uang-saku/',
+  url: './pagesikeren/uangsaku/uang-saku.html',
+},
+{
+  path: '/kehadiran/',
+  url: './pagesikeren/kehadiran/kehadiran.html',
+},
+{
+  path: '/raport/',
+  url: './pagesikeren/raport/raport.html',
+},
+{
+  path: '/kinerja-harian/',
+  url: './pagesikeren/kinerjaharian/kinerja-harian.html',
+}
 ];
