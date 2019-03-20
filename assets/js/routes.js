50221a94ac90c703e77f9b3513f47f842b35ddaa
mochajisukmawan <ajie.sukmawan@gmail.com>
2019-03-20 10:00:22 +0700

    simpe uang saku

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -91,30 +91,7 @@ var routes = [
       },
       pageAfterIn: function(event, page) {
         app.preloader.show();
-         var tanggal_us = $('#waktu').val();
-         var session = JSON.parse(localStorage.getItem("session"));
-         var nomor_register = session.nomor_register;
-        var datas = new FormData();
-        	datas.append("nomor_register", nomor_register);
-        	datas.append("bulan_tahun", tanggal_us);
-        	$.ajax({
-        		 type: "POST",
-        		 url: "http://10.64.5.40/sikeren/api/uang_saku",
-        		 data: datas,
-        		 processData: false,
-        		 contentType: false,
-        		 success: function(data) {
-        			 app.preloader.hide();
-        			 $('.us').html("Rp "+data.uang_saku);
-        			 $('.tp').html("Rp "+data.t_penampilan);
-        			 $('.tbks').html("Rp "+data.t_bpjs_kes);
-        			 $('.tbkn').html("Rp "+data.t_bpjs_ktn);
-        			 $('.um').html("Rp "+data.uang_makan);
-        			 $('.thp').html("Rp "+data.take_home_pay);
-        		 },
-        		 error: function(data) {
-        		 }
-        	 });
+        cari_us();
       },
       pageInit: function(event, page) {
         var no_registrasi = new FormData();
